396f1a85d82b421af6a62e2c9d267bc273e109cd
domniea <domniea@gmail.com>
2023-07-06 13:34:24 -0600

    Start RTV

---
+++ rlm-token-auth-series-context/routes/todoRouter.js
@@ -0,0 +1,82 @@
+const express = require("express")
+const todoRouter = express.Router()
+const Todo = require('../models/todo.js')
+
+// Get All Todos
+todoRouter.get("/", (req, res, next) => {
+  Todo.find((err, todos) => {
+    if(err){
+      res.status(500)
+      return next(err)
+    }
+    return res.status(200).send(todos)
+  })
+})
+
+//Get Singular Todo
+todoRouter.get('/:todoId', (re, res, next) => {
+  Todo.findOne({_id: req.params._id}, (err, data) => {
+    if(err) {
+      res.status(500)
+      return next(err)
+    }
+    return res.status(200).send(data)
+  })
+})
+
+//Get all Users todos
+todoRouter.get('/user', (req, res, next) => {
+  Todo.find({user: req.auth._id},(err, data) => {
+    if(err) {
+      res.status(500)
+      return next(err)
+    }
+    return res.status(200).send(data)
+  })
+})
+
+// Add new Todo
+todoRouter.post("/", (req, res, next) => {
+  req.body.user = req.auth._id
+  console.log(req.auth._id)
+  const newTodo = new Todo(req.body)
+  newTodo.save((err, savedTodo) => {
+    if(err){
+      res.status(500)
+      return next(err)
+    }
+    return res.status(201).send(savedTodo)
+  })
+})
+
+// Delete Todo
+todoRouter.delete("/:todoId", (req, res, next) => {
+  Todo.findOneAndDelete(
+    { _id: req.params.todoId, user: req.auth._id},
+    (err, deletedTodo) => {
+      if(err){
+        res.status(500)
+        return next(err)
+      }
+      return res.status(200).send(`Successfully deleted`)
+    }
+  )
+})
+
+// Update Todo
+todoRouter.put("/:todoId", (req, res, next) => {
+  Todo.findOneAndUpdate(
+    { _id: req.params.todoId, user: req.auth._id},
+    req.body,
+    { new: true },
+    (err, updatedTodo) => {
+      if(err){
+        res.status(500)
+        return next(err)
+      }
+      return res.status(201).send(updatedTodo)
+    }
+  )
+})
+
+module.exports = todoRouter
